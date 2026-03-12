10855886864410c3dede33713c53472c22a3b356
Maja <maja.bloom@outlook.com>
2026-03-12 14:51:48 +0100

    Add more basic code, Add menu, Run to make sure it works. Add comments that explain everything so far.

--- src/main/java/Contacts.java
+++ src/main/java/Contacts.java
@@ -1,20 +1,23 @@
 public class Contacts {
-//Create variables
-    // Private fields are where we store information
+
+// Declare private variable to store contact names.
+// Private fields are where information is stored
 private String name;
 private String number;
 
-//Constructor (like a start button for creating a contact)
+//Constructor (like a start button for adding a contact)
     public Contacts(String name, String number) {
         this.name = name;
         this.number = number;
     }
-    // Getter for Name. window lets other classes see the name
+// Getter for Name. Let's other classes read the name.
 public String getName() {
+        // Return value stored in name variable
         return name;
 }
 // Getter for number
 public String getNumber() {
+        // Return value stored in number variable
         return number;
     }
 }
