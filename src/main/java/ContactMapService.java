a6da590d0cec204e14abb817f08b17d691190e2b
Maja <maja.bloom@outlook.com>
2026-03-12 14:52:10 +0100

    Add more basic code, Add menu, Run to make sure it works. Add comments that explain everything so far.

--- src/main/java/ContactMapService.java
+++ src/main/java/ContactMapService.java
@@ -1,24 +1,44 @@
+// Import HashMap class for storage
+// Import Map interface to define storage type
 import java.util.HashMap;
 import java.util.Map;
-// List contact, update delete etc
 
+// Define class that handles contact logic
 public class ContactMapService {
     // Storage. HashMap where the Key is a String
     private Map<String, Contacts> contactMap = new HashMap<>();
-// Handles logic for adding a new contact.
 
+    // Adding a new contact.
     public void addContact(String name, String number) {
-// Check if the name (Key) already exists in our Map.
+        // Check if the name (Key) already exists in our Map.
         if (contactMap.containsKey(name)) {
-
             System.out.println("Error: A contact with the same name '" + name + "' already exists.");
         } else {
             // Create new object (instance) of the Contacts class.
             Contacts newContact = new Contacts(name, number);
-
             // Storage. Key = name, Value = number.
             contactMap.put(name, newContact);
             System.out.println("Sucess: Contact added!");
         }
     }
+
+    // List contacts
+    public void listContact() {
+    // Check if empty before trying to print it.
+        if (contactMap.isEmpty()) {
+            // Let's user know there is nothing to list.
+            System.out.println("The contact list is empty.");
+            // If map contains data, list it.
+        } else {
+            // Print contact list header
+            System.out.println("--- All Contacts ---");
+            // Loop through all Contact objects (c) stored in map values + print name and number using getter methods
+            for (Contacts c : contactMap.values()) {
+                System.out.println(c.getName() + " (" + c.getNumber() + ")");
+            }
+        }
+
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,51 @@
-// Call console helper
-
+import  java.util.Scanner;
 
 public class Main {
 
-    // Call the other classes from main, no other code necessary
-    // Call methods
-    /*
-    Switch case
-    not only a method call
-    put main menu here if you want, - or
-     */
-     */
+     public static void main(String[] args) {
 
-    */
-     */
+          // Connect to Service class
+          ContactMapService service = new ContactMapService();
+          // Create scanner to read input
+          Scanner scanner = new Scanner(System.in);
+          // Variable to start/end program
+          boolean running = true;
+
+          while (running) {
+               System.out.println("\n=== Contact Management ===");
+               System.out.println("1. Add contact");
+               System.out.println("2. Display all contacts");
+               System.out.println("0. Exit");
+               System.out.println("Choose an option: ");
+
+               // Read input
+               String choice = scanner.nextLine();
+
+               // Add contact
+               if (choice.equals("1")) {
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+
+                    System.out.print("Enter mobile: ");
+                    String mobile = scanner.nextLine();
+
+                    // Call for service class (logic)
+                    service.addContact(name, mobile);
+
+               // List contacts
+               } else if (choice.equals("2")) {
+                    service.listContact();
+
+               // Exit
+               } else if (choice.equals("0")) {
+                    running = false;
+                    System.out.println("Exiting program...");
+               } else {
+                    System.out.println("Invalid choice, try again.");
+               }
+
+          }
+
+     }
 
 }
